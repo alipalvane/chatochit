b50e358f8f1d7d534b533c1e968b8b43e795dcf1
Ali <alipalvane@gmail.com>
2023-11-29 13:51:12 +0330

    create context for auth user

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
 import { Form, Row, Col, Stack, Button, Alert } from "react-bootstrap";
+import { AuthContext } from "../context/AuthContext";
+
 const Register = () => {
+  const { registerInfo, upadteRegisterInfo } = useContext(AuthContext);
   return (
     <>
       <Form>
@@ -13,13 +17,25 @@ const Register = () => {
           <Col xs={6}>
             <Stack gap={3}>
               <h2>Register</h2>
-              <Form.Control type="text" placeholder="Name" />
-              <Form.Control type="email" placeholder="Email" />
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                onChange={(e) => upadteRegisterInfo({
+                  ...registerInfo, name:e.target.value
+                })}
+              />
+              <Form.Control type="email" placeholder="Email" onChange={(e) => upadteRegisterInfo({
+                  ...registerInfo, email:e.target.value
+                })}/>
+              <Form.Control type="password" placeholder="Password" onChange={(e) => upadteRegisterInfo({
+                  ...registerInfo, password:e.target.value
+                })}/>
               <Button variant="primary" type="submit">
                 Register
               </Button>
-              <Alert variant="danger"><p>An Error Occured !</p></Alert>
+              <Alert variant="danger">
+                <p>An Error Occured !</p>
+              </Alert>
             </Stack>
           </Col>
         </Row>
